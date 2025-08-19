deb5ad3e4929e7d36ee2f1d5c7f13fc29ad01feb
aluna <algimenez@fi.uba.ar>
2025-08-19 23:07:43 +0000

    add: title

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import QuoteDisplay from './components/Quote'
 import Categories from './components/Categories'
+import Title from './components/Title'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Title></Title>
       <Categories currentCategory={''} onCategoryChange={function (category: string): void {
         throw new Error('Function not implemented.')
       }}></Categories>
