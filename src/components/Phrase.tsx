cae50dfb6f3fbcc592e49899b4e532b6a45ea1ae
Esteban <estebankaraputny18@gmail.com>
2025-08-19 20:09:25 -0300

    :sparkles: Add: styles in the components

--- src/components/Phrase.tsx
+++ src/components/Phrase.tsx
@@ -1,16 +1,20 @@
 import './stylesComponents/Phrase.css';
+import { CiHeart } from "react-icons/ci";
+import { FaRotate } from "react-icons/fa6";
 
 
 const Phrase = ({ phrase, author }: { phrase: string, author: string }) => {
     return (
-        <div className="phrase">
-            <div className="phrase-controls">
-                <button>Nueva Frase</button>
-                <button>Añadir a favoritos</button>
+        <>  
+            <div className="phrase-container">
+                <p className='phrase'>{phrase}</p>
+                <p>{author}</p>
+                <div className="phrase-controls">
+                    <button className="button-phrase"><FaRotate /> Nueva Frase</button>
+                    <button className="button-phrase"><CiHeart /> Añadir a favoritos</button>
+                </div>  
             </div>
-            <p>{phrase}</p>
-            <p>{author}</p>
-        </div>
+        </>
     );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,6 @@
 import Phrase from '../components/Phrase';
 
 
-
-
 const Home = () => {   
     return (
         <>
@@ -10,9 +8,9 @@ const Home = () => {
             <h1>Welcome to the Home Page</h1>
             <p>This is the main content of the home page.</p>
             </div>
-            <Phrase 
-            phrase="The only limit to our realization of tomorrow is our doubts of today." 
-            author="- Franklin D. Roosevelt" 
+                <Phrase 
+                phrase="The only limit to our realization of tomorrow is our doubts of today." 
+                author="-Franklin D. Roosevelt" 
             />
         </>
     );
