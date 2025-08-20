8fb74710c3fdf8dd6eff26724014bf861c02e317
aluna <algimenez@fi.uba.ar>
2025-08-20 00:46:50 +0000

    add: filters

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
-import { useState } from 'react'
+import { useState, type QuoteHTMLAttributes } from 'react'
 import './App.css'
 import QuoteDisplay from './components/Quote'
 import Categories from './components/Categories'
 import Title from './components/Title'
+import Phrases from '../public/phrase.json';
+
+
+type PhraseProps = {
+  id: number;
+  phrase: string;
+  author: string;
+  category: string;
+  onNextPhrase: () => void;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [currentCategory, setCurrentCategory] = useState<string>('Todas');
+
+  const quotes = Phrases;
+
+  const filteredQuotes = currentCategory === 'Todas'
+    ? quotes
+    : quotes.filter((quote: { category: string }) => quote.category === currentCategory);
+
+  const randomIndex = Math.floor(Math.random() * filteredQuotes.length);
 
   return (
     <>
       <Title></Title>
+
       <Categories currentCategory={''} onCategoryChange={function (category: string): void {
         throw new Error('Function not implemented.')
       }}></Categories>
@@ -21,6 +41,20 @@ function App() {
       }}>
 
       </QuoteDisplay>
+
+      <div>
+        {filteredQuotes.length > 0 && (
+          <div>
+            <blockquote>
+              <p>{filteredQuotes[randomIndex].text}</p>
+              <footer>— {filteredQuotes[randomIndex].author}</footer>
+            </blockquote>
+            {/* los botones */}
+          </div>
+        )}
+      </div>
+
+
     </>
   )
 }
